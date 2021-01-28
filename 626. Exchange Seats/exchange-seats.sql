f34066491a3456d0940e5fd22d955a971aec68e3
Jerry Cheng <dlgs5100@gmail.com>
2021-01-28 14:48:42 +0800

    Update exchange-seats.sql

--- 626. Exchange Seats/exchange-seats.sql
+++ 626. Exchange Seats/exchange-seats.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
 SELECT (CASE 
-    WHEN mod(id,2) = 1 AND id = counts THEN id
-    WHEN mod(id,2) = 1 AND id != counts THEN id+1
-    ELSE id-1 
-    END) AS id, student
+    WHEN mod(id,2) = 1 AND id = t.counts THEN id
+    WHEN mod(id,2) = 1 AND id != t.counts THEN id+1
+    ELSE id-1 
+    END) AS id, student
 FROM 
-    seat, (SELECT COUNT(*) AS counts FROM seat) AS counts
+    seat, (SELECT COUNT(*) AS counts FROM seat) AS t
 ORDER BY
-    id;
+    id;
