d905186ae6eca264b2658c51eec759477fbca461
Jerry Cheng <dlgs5100@gmail.com>
2021-02-08 12:46:26 +0800

    Time: 4 ms (98.56%), Space: 6.3 MB (95.98%) - LeetHub

---
+++ 53. Maximum Subarray/maximum-subarray.c
@@ -0,0 +1,11 @@
+int maxSubArray(int* nums, int numsSize) {
+    int i, sum = 0, max = -pow(10,5);
+    for (i = 0; i < numsSize; i++) {
+        sum += *(nums + i);
+        if (sum >= max)
+            max = sum;
+        if (sum < 0)
+            sum = 0;
+    }
+    return max;
+}
