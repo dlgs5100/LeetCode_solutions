a1ac3a43fe8c1951113a66fca15cbea8a6b4009c
Jerry Cheng <dlgs5100@gmail.com>
2021-03-05 17:16:11 +0800

    Time: 4 ms (86.81%), Space: 6.1 MB (63.63%) - LeetHub

--- 21. Merge Two Sorted Lists/merge-two-sorted-lists.c
+++ 21. Merge Two Sorted Lists/merge-two-sorted-lists.c
@@ -5,22 +5,20 @@
  *     struct ListNode *next;
  * };
  */
-​
-​
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
-    struct ListNode *l3_head = (struct ListNode*)malloc(sizeof(struct ListNode));
-    l3_head->next = NULL;
-    struct ListNode *l3 = l3_head;
+    struct ListNode *head = malloc(sizeof(struct ListNode)), *cur;
+    cur = head;
+    cur->next = NULL;
     while(l1 || l2){
-        if((l1 && !l2) || (l1 && l2 &&(l1->val <= l2->val))){
-            l3->next = l1;
+        if(!l2 || (l1 && l1->val <= l2->val)){
+            cur->next = l1;
             l1 = l1->next;
         }
-        else if((!l1 && l2) || (l1 && l2 &&(l1->val > l2->val))){
-            l3->next = l2;
+        else{
+            cur->next = l2;
             l2 = l2->next;
         }
-        l3 = l3->next;
+        cur = cur->next;
     }
-    return l3_head->next; 
+    return head->next;
 }
