4dcb90f9d4bcbbb3d15c637382d54aaf139ae4d7
Jerry Cheng <dlgs5100@gmail.com>
2021-03-05 15:06:19 +0800

    Time: 4 ms (48.26%), Space: 6.2 MB (19.19%) - LeetHub

--- 14. Longest Common Prefix/longest-common-prefix.c
+++ 14. Longest Common Prefix/longest-common-prefix.c
@@ -1,13 +1,26 @@
 char * longestCommonPrefix(char ** strs, int strsSize){
-    if(strsSize==0)
+    int longestIndex = -1, i;
+    char sameChar;
+    bool allSame;
+    if(strsSize == 0)
         return "";
-    char* result = strs[0];
-    int i,j;
-    for(i=1; i<strsSize; i++){
-        j=0;
-        while(result[j]&&strs[i][j]&&result[j]==strs[i][j])
-            j++;
-        result[j]='\0';
+    while(1){
+        if(longestIndex+1 >= strlen(strs[0]))
+            break;
+        allSame = true;
+        sameChar = strs[0][longestIndex+1];
+        for(i=1; i<strsSize; i++){
+            if(sameChar != strs[i][longestIndex+1]){
+                allSame = false;
+                break;
+            }
+        }
+        printf("%c\n", sameChar);
+        if(allSame)
+            longestIndex++;
+        else
+            break;
     }
-    return result;
+    strs[0][longestIndex+1] = '\0';
+    return strs[0];
 }
