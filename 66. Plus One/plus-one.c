19af25ee4c4a93b5c07829fc32d7c7b39ff68b4b
Jerry Cheng <dlgs5100@gmail.com>
2021-03-06 17:13:01 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (60.13%) - LeetHub

--- 66. Plus One/plus-one.c
+++ 66. Plus One/plus-one.c
@@ -2,23 +2,18 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* plusOne(int* digits, int digitsSize, int* returnSize){
-    int* result = (int*)calloc(digitsSize + 1, sizeof(int));
-    *returnSize = digitsSize;
-    int i, num, carry = 1;
-    for (i = digitsSize - 1; i >= 0; i--) {
-        num = *(digits + i) + carry;
-        if (num >= 10) {
-            num %= 10;
-            carry = 1;
-        }
-        else
-            carry = 0;
-        *(result + i + 1) = num;
-    }
-    if (carry == 1) {
-        *result = 1;
-        (*returnSize)++;
-        return result;
-    }
-    return result + 1;
+    int i, carry = 1;
+    int *result = malloc(sizeof(int)*(digitsSize+1));
+    for(i=digitsSize-1; i>=0; i--){
+        result[i+1] = digits[i]+carry;
+        carry = result[i+1]/10;
+        result[i+1] %= 10;
+    }
+    if(carry == 1){
+        result[0] = carry;
+        *returnSize = digitsSize+1;
+        return result;
+    }
+    *returnSize = digitsSize;
+    return result+1;
 }
